cc4c9074885adcf933f9ed1b6fdac02e66256d49
wangjie <wjnovember@qq.com>
2020-04-19 23:31:46 +0800

    备份从零设计引导框架-实操篇

---
+++ source/_posts/GuideMgr02/GuideMgr.lua
@@ -0,0 +1,24 @@
+GuideMgr = class("GuideMgr")
+
+GuideMgr._guideId = nil
+GuideMgr._finished = {}
+
+function GuideMgr.checkStart(point, guideId)
+
+end
+
+function GuideMgr.finish()
+
+end
+
+function GuideMgr.start()
+
+end
+
+function GuideMgr.next()
+
+end
+
+function GuideMgr.getInfo()
+
+end
